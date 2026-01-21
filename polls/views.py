f3075485e586329daa5054e20461e0ee2cf525c7
shakhrillodev <shaxrillonusratov@gmail.com>
2026-01-21 10:17:41 +0500

    feat:reload

--- polls/views.py
+++ polls/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index .test")
+    return HttpResponse("Hello, world. You're at the polls index .test. test2")
