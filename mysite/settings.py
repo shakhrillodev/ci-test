8f7df972fdfced4c1c16fb138bd47c04235ac595
shakhrillodev <shaxrillonusratov@gmail.com>
2026-01-21 09:29:47 +0500

    change

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,3 +120,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['shaxrillo.pythonanywhere.com']
+
+DEBUG = False
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for mysite project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+path = '/home/shaxrillo/ci-test'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'ci-test.settings'
+)
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
 application = get_wsgi_application()
